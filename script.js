6f6031565779d757452f90d15a61a287fd1dee7c
Miyukiana <weber.selina@icloud.com>
2025-04-14 21:46:14 +0200

    .

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function newTd() {
-const addElement = document.querySelector("input").value.trim();
-if (!addElement) {
-    alert("Please enter a valid task to do.");
-    return;
-}
-const newElement = document.createElement("li");
-document.querySelector("ol").appendChild(newElement);
-newElement.textContent = addElement;
+    const addElement = document.querySelector("input").value.trim();
+    if (!addElement) {
+        alert("Please enter a valid task to do.");
+        return;
+    }
+    const newElement1 = document.createElement("li");
+    const newElement2 = document.createElement("input");
+    newElement2.type = "checkbox";
+    newElement1.appendChild(newElement2);
+    newElement1.appendChild(document.createTextNode(addElement + " "));
+    document.querySelector("ul").appendChild(newElement1);
 }
 function deleteAll(){
     const toDoList = document.querySelectorAll("li");
