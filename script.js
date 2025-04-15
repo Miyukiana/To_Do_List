9380893315a6e8136c3e671ccd89c4164cfe0d77
Miyukiana <weber.selina@icloud.com>
2025-04-15 17:07:33 +0200

    check all function

--- script.js
+++ script.js
@@ -7,11 +7,25 @@ function newTd() {
     const newElement1 = document.createElement("li");
     const newElement2 = document.createElement("input");
     newElement2.type = "checkbox";
+    // add Event: if checkbox is checked, li is moved to doneList
+    newElement2.addEventListener("change", function() {
+        if (newElement2.checked) {
+            document.getElementById("doneList").appendChild(newElement1);
+        } else {
+            document.getElementById("toDoList").appendChild(newElement1);
+        }
+    });
     newElement1.appendChild(newElement2);
     newElement1.appendChild(document.createTextNode(addElement + " "));
-    document.querySelector("ul").appendChild(newElement1);
+    document.getElementById("toDoList").appendChild(newElement1);
 }
 function deleteAll(){
     const toDoList = document.querySelectorAll("li");
 toDoList.forEach(li => li.remove());
+}
+function checkAll(){
+    document.querySelectorAll("input[type='checkbox']").forEach(checkbox => {
+        checkbox.checked = true;
+        checkbox.dispatchEvent(new Event('change')); //Trigger change event to move to doneList
+    });
 }
