1dc6d8c02c804be826bd87241dcccbf964abdf68
Miyukiana <weber.selina@icloud.com>
2025-04-16 13:26:01 +0200

    color added

--- script.js
+++ script.js
@@ -10,10 +10,22 @@ function newTd() {
     const newElement2 = document.createElement("input");
     newElement2.type = "checkbox";
 
+    // li einfärben
+    newElement1.style.backgroundColor = "rgb(122, 209, 122)";
+    newElement1.style.border = "solid 0.5px rgb(38, 174, 38)";
+    newElement1.style.margin = "5px";
+    newElement1.style.borderRadius = "5px";
+
     // Wenn Checkbox geändert → <li> verschieben
     newElement2.addEventListener("change", function () {
         if (newElement2.checked) {
             document.getElementById("doneList").appendChild(newElement1);
+            // li anders einfärben
+            newElement1.style.backgroundColor = " rgb(241, 152, 152)";
+            newElement1.style.border = "solid 0.5px rgb(202, 51, 51)";
+            newElement1.style.textDecoration = "line-through";
+            newElement1.style.textDecorationColor = "red";
+            newElement1.style.margin = "5px";
         } else {
             document.getElementById("toDoList").appendChild(newElement1);
         }
@@ -38,14 +50,15 @@ function newTd() {
     document.getElementById("toDoList").appendChild(newElement1);
 
     // Rechtsklick zeigt Menü (Desktop)
-    newElement1.addEventListener("contextmenu", function (e) {
-        e.preventDefault();
-        closeAllMenus();
-        contextMenu.style.display = "block";
-        contextMenu.style.left = e.offsetX + "px";
-        contextMenu.style.top = e.offsetY + "px";
+    newElement1.addEventListener("touchstart", (e) => {
+        touchTimer = setTimeout(() => {
+            closeAllMenus();
+            contextMenu.style.display = "block";
+            contextMenu.style.left = e.touches[0].clientX + "px";
+            contextMenu.style.top = e.touches[0].clientY + "px";
+        }, 600);
     });
-
+newElement1.addEventListener("touchend", () => clearTimeout(touchTimer));    
     // Langes Drücken zeigt Menü (Mobile)
     let touchTimer;
     newElement1.addEventListener("touchstart", () => {
